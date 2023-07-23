d962e8135f12bbc9d1529ecab662c76ba0439ee1
lera2342 <valeriiapylypenko3@gmail.com>
2023-07-23 21:20:23 +0300

    4-commit

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,44 +1,45 @@
-import throttle from "lodash.throttle";
+import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
 const { email, message } = form.elements;
+const localData = localStorage.getItem('feedback-form-state'); 
 
 form.addEventListener('input', throttle(inStorage, 500));
-
-const dataLocal = localStorage.getItem('feedback-form-state');
-PageReload();
+pageReload();
 
 form.addEventListener('submit', onSubmit);
 
-function inStorage() {
-    const data = {
-        email: email.value,
-        message:message.value
-    }
-    localStorage.setItem('feedback-from-state', JSON.stringify(data));
-}
-
 function onSubmit(event) {
     event.preventDefault();
-    if ((email.value.length && message.value.length) < 1) {
-        alert('All fields must be filled');
-    } else {
-        const data = {
-            email: email.value,
-            message: message.value
-        }
-        console.log(data);
-        event.currentTarget.reset();
-        localStorage.removeItem('feedback-form-state');
+     if ((email.value.length && message.value.length) < 1) {
+         alert('You must write something');
+     } else {
+         const data = {
+             email: email.value,
+             message: message.value
+         }
+         console.log(data);
+         event.currentTarget.reset();
+         localStorage.removeItem('feedback-form-state');
     }
+
 }
 
-function PageReload() {
-    if (dataLocal) {
-        email.value = JSON.parse(dataLocal).email;
-        message.value = JSON.parse(dataLocal).message;
+function inStorage() {
+    const data = {
+            email: email.value,
+            message: message.value
+    }
+    localStorage.setItem('feedback-form-state', JSON.stringify(data));
+}
+
+function pageReload() {
+    if (localData) {
+        email.value = JSON.parse(localData).email;
+        message.value = JSON.parse(localData).message;
     } else {
         email.value = '';
         message.value = '';
     }
+
 }
